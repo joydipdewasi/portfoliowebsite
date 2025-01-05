6157c911cee39f92d16fe99e537fd263cab3cc7f
joydipdewasi <dewasijoydip@gmail.com>
2025-01-05 21:55:07 +0530

    heroScetion file is animated

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -3,13 +3,19 @@
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
 
 const HeroSection = () => {
   return (
     <div>
       <section className="lg:py-16">
         <div className="grid grid-cols-1 sm:grid-cols-12">
-          <div className="col-span-8 place-self-center text-center sm:text-left justify-seif-start">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            className="col-span-8 place-self-center text-center sm:text-left justify-seif-start"
+          >
             <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold lg:leading-normal">
               <span className="text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-pink-600">
                 Hello, I'm{' '}
@@ -46,10 +52,20 @@ const HeroSection = () => {
                 </span>
               </button>
             </div>
-          </div>
+          </motion.div>
           {/* -------image section------ */}
-          <div className="col-span-4 place-self-center mt-4 lg:mt-0">
-            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            className="col-span-4 place-self-center mt-4 lg:mt-0"
+          >
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }}
+              className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative"
+            >
               <Image
                 src="/me.png"
                 alt="me image"
@@ -58,8 +74,8 @@ const HeroSection = () => {
                 height={370}
                 priority
               />
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
       </section>
     </div>
